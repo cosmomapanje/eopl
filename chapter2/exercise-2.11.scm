0d900e1d475aed4732f2e737579f821cb90438e7
cosmomapanje <cosmo.mapanje@gmail.com>
2016-05-20 20:05:15 +0800

    charpter2 : exercise2.11.scm

---
+++ chapter2/exercise-2.11.scm
@@ -0,0 +1,54 @@
+;;error
+(define error
+  (lambda (msg)
+    (display msg)))
+
+;;empty-env
+(define empty-env
+ (lambda ()
+   '()))
+
+;;extend-env*
+(define extend-env*
+  (lambda (vars vals env)
+    (cond
+      ((list? vars)
+       (if (list? vals)
+           (cons (cons vars vals) env)
+           (error 'args-doesnt-match)))
+      (else
+       (cons (cons (list vars) (list vals)) env)))))
+
+;;search-in-list
+(define search-in-list
+  (lambda (var list-vars list-vals)
+    (cond 
+      ((null? (car list-vars))
+       (cons #f '()))
+      ((eqv? (car list-vars) var)
+       (cons #t (car list-vals)))
+      (else
+       (search-in-list var (cdr list-vars) (cdr list-vals))))))
+
+;;apply-env
+(define apply-env
+  (lambda (env search-var)
+    (cond
+      ((eqv? env empty-env)
+       (error 'no-bind))
+      (else
+       (let ((saved-vars (car (car env)))
+             (saved-vals (cdr (car env)))
+             (saved-env (cdr env)))
+         (let ((searched (search-in-list search-var saved-vars saved-vals)))
+           (if (car searched)
+               (cdr searched)
+               (apply-env saved-env search-var))))))))
+
+;;
+(cons (cons '(1 2 3) '(x y z)) '('(a) 1))
+(cons (list 1) (list 'a))
+(define e (extend-env* '(a b c) '(1 2 3)
+                       (extend-env* 'd 4
+                                    (extend-env* '(e f) '(5 6) empty-env))))
+(extend-env* 'a 1 empty-env)

57ea690ae9af401ab25e1b096d9acc39e16256df
cosmomapanje <cosmo.mapanje@gmail.com>
2016-05-29 21:16:27 +0800

    Chapter2 exercise

--- chapter2/exercise-2.15.scm
+++ chapter2/exercise-2.15.scm
@@ -1,4 +1,57 @@
-;;;Grammar Rules
-;; 1.Include one constructor for each kind of data in the data type.
-;; 2.Include one predicate for each kind of data in the data type.
-;; 3.Include one extractor for each piece of data passed to a constructor of the data type.
+;;; Exercise 2.15
+;;; var-exp
+;;; lambda-exp
+;;; app-exp
+
+;;; var-exp?
+;;; lambda-exp?
+;;; app-exp?
+(define var-exp?
+  (lambda (lc-exp)
+	(symbol? lc-exp)))
+
+(define lambda-exp?
+  (lambda (lc-exp)
+	(eqv? (car lc-exp) 'lambda)))
+
+(define app-exp?
+  (lambda (lc-exp)
+	(and (not (var-exp? lc-exp))
+		 (not (lambda-exp? lc-exp)))))
+
+;;; var-exp->var
+;;; lambda-exp->bound-var
+;;; lambda-exp->body
+;;; app-exp->rator
+;;; app-exp->rand
+(define var-exp->var
+  (lambda (lc-exp)
+	lc-exp))
+
+(define lambda-exp->bound-var
+  (lambda (lc-exp)
+	(car (car (cdr lc-exp)))))
+
+(define lambda-exp->body
+  (lambda (lc-exp)
+	(car (cdr (cdr lc-exp)))))
+
+(define app-exp->rator
+  (lambda (lc-exp)
+	(car lc-exp)))
+
+(define app-exp->rand
+  (lambda (lc-exp)
+	(car (cdr lc-exp))))
+
+(define occurs-free?
+  (lambda (search-var exp)
+	(cond
+	 ((var-exp? exp) (eqv? search-var (var-exp->var exp)))
+	 ((lambda-exp? exp)
+	  (and
+	   (not (eqv? search-var (lambda-exp->bound-var exp)))
+	   (occurs-free? search-var (lambda-exp->body exp))))
+	 (else
+	  (or (occurs-free? search-var (app-exp->rator exp))
+		  (occurs-free? search-var (app-exp->rand exp)))))))
---
+++ chapter2/exercise-2.19.scm
@@ -0,0 +1,21 @@
+;;; Exercise 2.19
+(define number->bintree
+  (lambda num))
+
+(define current-element
+  (lambda bintree))
+
+(define move-to-left-son
+  (lambda bintree))
+
+(define move-to-right-son
+  (lambda bintree))
+
+(define at-leaf?
+  (lambda (bintree)))
+
+(define insert-to-left
+  (lambda (num bintree)))
+
+(define insert-to-right
+  (lambda (num bintree)))

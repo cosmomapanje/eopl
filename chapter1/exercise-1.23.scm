00f14f9e9a9c30f8d24939bdede3f0a9b7ba2afc
cosmomapanje <cosmo.mapanje@gmail.com>
2016-06-05 22:53:07 +0800

    Exercise 1.23

--- chapter1/exercise-1.23.scm
+++ chapter1/exercise-1.23.scm
@@ -1,8 +1,16 @@
 ;;; Exercise 1.23
+(define list-index-with-n
+  (lambda (pred lst n)
+    (cond ((null? lst) #f)
+          (else
+           (if (pred (car lst))
+               n
+               (list-index-with-n pred (cdr lst) (+ 1 n)))))))
+
 (define list-index
   (lambda (pred lst)
-	(cond ((null? lst) #f)
-		  (else
-		   (if (pred (car lst))
-			   0
-			   (+ 1 (list-index pred (cdr lst))))))))
+    (list-index-with-n pred lst 0)))
+
+(list-index number? '(a 2 (1 3) b 7))
+(list-index symbol? '(a (b c) 17 foo))
+(list-index symbol? '(1 2 (a b) 3))
